46e9fba4c86ae3716d5f2999cdab7da9d9cd020f
Adam Kiezun <akiezun@gmail.com>
2015-06-24 11:29:32 -0400

    API fixed to use interfaces

--- src/java/htsjdk/variant/variantcontext/VariantContextUtils.java
+++ src/java/htsjdk/variant/variantcontext/VariantContextUtils.java
@@ -241,7 +241,7 @@ public class VariantContextUtils {
         return VariantContextUtils.initializeMatchExps(map);
     }
 
-    public static List<JexlVCMatchExp> initializeMatchExps(ArrayList<String> names, ArrayList<String> exps) {
+    public static List<JexlVCMatchExp> initializeMatchExps(List<String> names, List<String> exps) {
         String[] nameArray = new String[names.size()];
         String[] expArray = new String[exps.size()];
         return initializeMatchExps(names.toArray(nameArray), exps.toArray(expArray));
